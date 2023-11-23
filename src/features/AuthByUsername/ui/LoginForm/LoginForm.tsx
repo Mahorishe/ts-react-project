233844955764a4144ea06cd3ed2c463415b4d4df
Sergey Zhirnov <mahorishe73@gmail.com>
2023-11-23 11:35:41 +0300

    1.Добавлено модальное окно авторизации с lazy монитрвоанием в DOM. 2.Реализован компонент Input

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -17,7 +17,7 @@ export const LoginForm = (props: LoginFormProps) => {
 
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
-            <Input type="text" placeholder="Введите никнейм" className={cls.inputForm} />
+            <Input type="text" placeholder={t('Введите никнейм')} className={cls.inputForm} />
             <Input type="text" className={cls.inputForm} />
             <Button className={cls.loginBtn}>{t('Войти')}</Button>
         </div>
